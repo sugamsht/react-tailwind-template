1b1db1802e9b17bf3327c628b7fe053841031094
Sugam Shrestha <sugamsht143@gmail.com>
2022-06-29 22:47:17 +0545

    React Tailwindcss Template

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="w-screen h-screen">
+      <h1 className="flex justify-center items-center text-3xl text-gray-800 font-bold underline my-5">
+        Begin your React journey with Tailwindcss!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
